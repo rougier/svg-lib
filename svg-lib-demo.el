cca504cbfd2ac82caa2ee9af865f3a5840b3f136
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2023-06-17 08:06:23 +0200

    Added date example

--- svg-lib-demo.el
+++ svg-lib-demo.el
@@ -41,7 +41,7 @@
           
 
 (insert-image (svg-lib-progress-bar 0.75 nil :radius 8 :stroke 2 :padding 0))
-  
+ 
 
 (dotimes (i 10)
   (insert-image (svg-lib-progress-pie (/ (+ i 1) 10.0) nil
@@ -56,13 +56,14 @@
                               :font-family "Roboto Mono"
                               :font-weight 500
                          :stroke 0 :background "#673AB7" :foreground "white"))
-  
+ 
 
 (insert-image (svg-lib-icon "gnuemacs" nil :collection "simple"
                             :stroke 0 :scale 1 :padding 0))
+  
+
+(insert-image (svg-lib-date nil nil :font-family "Roboto" :radius 5
+                                    :foreground "#673AB7"))
  
-
-(insert-image (svg-lib-date nil nil :foreground "#673AB7"))
-
  
 
